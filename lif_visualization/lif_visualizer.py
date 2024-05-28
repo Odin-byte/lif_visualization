bbe8dff486382ce05e77f43cd6f8d99a687d45d2
Odin-byte <dominikwerman@gmail.com>
2024-05-28 16:25:48 +0200

    Added visual representations of LIF stations.

--- lif_visualization/lif_visualizer.py
+++ lif_visualization/lif_visualizer.py
@@ -15,6 +15,7 @@ class LIF_Visualizer:
         self.current_file = ""
         self.lif_data = {}
         self.layouts = {}
+        self.stationDict = {}
         return
 
     def load_from_file(self, filepath: str) -> None:
@@ -42,16 +43,55 @@ class LIF_Visualizer:
         # Create a graph
         G = nx.DiGraph()
 
+        # Add nodes and edges to the graph
+        self._add_nodes_to_graph(layout, G)
+        self._add_edges_to_graph(layout, G)
+
+        pos = nx.get_node_attributes(G, "pos")
+        colors_edges = nx.get_edge_attributes(G, "color").values()
+        colors_nodes = nx.get_node_attributes(G, "color").values()
+        weights = nx.get_edge_attributes(G, "weight").values()
+
+        fig, ax = plt.subplots()
+
+        nx.draw(
+            G,
+            pos,
+            ax,
+            edge_color=colors_edges,
+            width=list(weights),
+            node_color=colors_nodes,
+            with_labels=True,
+            connectionstyle="Arc3, rad=0.02",
+        )
+
+        self._add_stationTexts_to_graph(pos, ax)
+        plt.show()
+
+    def _add_nodes_to_graph(self, layout: dict, graph: nx.Graph) -> None:
         # Extract the list of nodes from the layout
         nodes = layout["nodes"]
+
+        self._fill_stationDict(layout["stations"])
+
+        print(self.stationDict)
 
         # Loop over all nodes within the layout
         for node in nodes:
             # Add the pair nodeId / nodePosition to the dict
             x = node["nodePosition"]["x"]
             y = node["nodePosition"]["y"]
-            G.add_node(node["nodeId"], pos=(x, y))
 
+            # Check if the node has a station associated with it
+            if node["nodeId"] in self.stationDict:
+                nodeColor = "green"
+            else:
+                nodeColor = "grey"
+            graph.add_node(node["nodeId"], pos=(x, y), color=nodeColor)
+
+        return
+
+    def _add_edges_to_graph(self, layout: dict, graph: nx.Graph) -> None:
         # Extract the list of edges from the layout
         edges = layout["edges"]
 
@@ -64,19 +104,38 @@ class LIF_Visualizer:
                 color = "red"
             else:
                 color = "black"
-            G.add_edge(edge["startNodeId"], edge["endNodeId"], color=color, weight=2)
-            print(edge)
+            graph.add_edge(
+                edge["startNodeId"], edge["endNodeId"], color=color, weight=2
+            )
 
-        pos = nx.get_node_attributes(G, "pos")
-        colors = nx.get_edge_attributes(G, "color").values()
-        weights = nx.get_edge_attributes(G, "weight").values()
+        return
 
-        nx.draw(
-            G,
-            pos,
-            edge_color=colors,
-            width=list(weights),
-            with_labels=True,
-            connectionstyle="Arc3, rad=0.02",
-        )
-        plt.show()
+    def _fill_stationDict(self, stations: dict) -> None:
+        print(stations)
+        # Create dict
+        stationDict = {}
+
+        # Loop over stations and add key-value pairs
+        for station in stations:
+            for node in station["interactionNodeIds"]:
+
+                # Key: nodeId - Value: stationName
+                stationDict[node] = station["stationName"]
+
+        self.stationDict = stationDict
+
+    def _add_stationTexts_to_graph(self, positions: dict, ax) -> None:
+        for station, description in self.stationDict.items():
+            x, y = positions[station]
+            ax.annotate(
+                description,
+                xy=(x, y),
+                xytext=(5, 20),
+                textcoords="offset points",
+                bbox=dict(facecolor="green", alpha=0.4),
+                fontsize=8,
+                horizontalalignment="center",
+                verticalalignment="center",
+            )
+
+        return
